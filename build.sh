f61116cf0800bd112d16c05e9f567a1dad20aa2e
niant <antti.niemenpaa@gmail.com>
2014-10-01 22:38:58 +0300

    Add build script.

---
+++ build.sh
@@ -0,0 +1,24 @@
+ONCE_VER=v$(grep version package.json | cut -d':' -f2 | cut -d'"' -f2 | sort -g -r | head -1)
+ONCE_VERSIONS=$(git ls-remote --tags)
+
+if [[ $ONCE_VERSIONS == *$ONCE_VER* ]]
+then
+  git config --global user.name "niant"
+
+  git checkout -b release
+
+  rm -rf .sass-cache/
+  rm -rf node-modules/
+  cp -r dist/css/* css/
+  cp -r src/sass/* sass/
+  rm -rf dist/
+  rm -rf src/
+
+  git add .
+  git add docs/
+  git commit -am "New version build $ONCE_VER"
+  git tag $ONCE_VER -m "New version $ONCE_VER"
+  git push origin $ONCE_VER
+  git checkout master
+  git branch -D release
+fi
