cea0561173dc8b7aa2700041ef4e869759b89bf4
niant <antti.niemenpaa@gmail.com>
2017-01-28 19:21:55 +0200

    Move npm publish into build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-ONCE_VER=v$(grep version package.json | cut -d':' -f2 | cut -d'"' -f2 | sort -g -r | head -1)
+ONCE_VER=$(grep version package.json | cut -d':' -f2 | cut -d'"' -f2 | sort -g -r | head -1)
 ONCE_VERSIONS=$(git ls-remote --tags)
 
 if [[ $ONCE_VERSIONS != *$ONCE_VER* ]]
@@ -34,6 +34,11 @@ then
   git commit -am "New version build $ONCE_VER"
   git tag $ONCE_VER -m "New version $ONCE_VER"
   git push origin $ONCE_VER
+
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+  npm publish --tag $ONCE_VER
+
+  rm .npmrc
   git checkout master
   git branch -D release
 fi
